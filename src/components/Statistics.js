740e01d8dc5fdf273c9fde0ec2ed4ef4748f4a67
Konurbaeva <konurbaevamadina@gmail.com>
2022-05-15 20:38:49 +0200

    removed comments from Statistics component

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,11 +1,6 @@
 function Statistics({ good, neutral, bad, total, positivePercentage }) {
     return (
         <>
-            {/* <div>Good: {good}</div>
-         <div>Neutral:{neutral}</div>
-         <div>Bad: {bad}</div>
-          <span>Total:{total} </span>
-          <div>Positive feedback: {this.countPositiveFeedbackPercentage(total, good)} %</div> */}
             <div>Statistics</div>
             <div>Good: {good}</div>
             <div>Neutral:{neutral}</div>
