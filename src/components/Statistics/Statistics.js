65b294e8b5b39d14e38081f39b12ea5a8cb6773c
Konurbaeva <konurbaevamadina@gmail.com>
2022-05-19 14:43:31 +0200

    added total props

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -16,6 +16,7 @@ Statistics.propTypes = {
     good: PropTypes.number,
     neutral: PropTypes.number,
     bad: PropTypes.number,
+    total: PropTypes.number,
     positivePercentage: PropTypes.number,
 };
 
